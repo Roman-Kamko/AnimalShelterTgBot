ab9c9f72f2a3138ae334be5ffdb022b5ee110479
RomanKamko <r.kamko@mail.ru>
2023-08-25 21:33:52 +0400

    исправлена сущность Shelter, добавлена связь OneToMany на сущность Animal

--- src/main/java/com/team2/animalshelter/entity/Shelter.java
+++ src/main/java/com/team2/animalshelter/entity/Shelter.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -18,9 +15,16 @@ public class Shelter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String address;
+
     private String phoneNumber;
+
     private String timeTable;
+
+    @OneToMany(mappedBy = "shelter")
+    private List<Animal> animals;
 
 }
