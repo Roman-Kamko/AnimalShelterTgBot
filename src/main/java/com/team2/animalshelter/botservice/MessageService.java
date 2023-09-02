42e7ca483836c3151e9fe8cfc48c42e8375d031c
RomanKamko <r.kamko@mail.ru>
2023-09-02 21:31:02 +0400

    Добавлены комментарии Добавлено поле breed в сущность Animal Написана миграция на добавление столбца breed в таблицу animal

--- src/main/java/com/team2/animalshelter/botservice/MessageService.java
+++ src/main/java/com/team2/animalshelter/botservice/MessageService.java
@@ -6,6 +6,9 @@ import com.team2.animalshelter.constant.Information;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Класс для отправки сообщений пользователю
+ */
 @Service
 @RequiredArgsConstructor
 public class MessageService {
