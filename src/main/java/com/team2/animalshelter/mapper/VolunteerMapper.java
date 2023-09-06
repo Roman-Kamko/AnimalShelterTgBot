1f0db7ba85908516d478ac9d7d12a7a52cbfacd6
nikita <7jay94@mail.ru>
2023-09-06 17:09:40 +0200

    feat: добавил AnimalMapper!

--- src/main/java/com/team2/animalshelter/mapper/VolunteerMapper.java
+++ src/main/java/com/team2/animalshelter/mapper/VolunteerMapper.java
@@ -2,19 +2,25 @@ package com.team2.animalshelter.mapper;
 
 import com.team2.animalshelter.dto.in.VolunteerDtoIn;
 import com.team2.animalshelter.entity.Volunteer;
+import com.team2.animalshelter.repository.ShelterRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class VolunteerMapper {
 
-
+    final ShelterRepository shelterRepository;
 
     public Volunteer toEntity(VolunteerDtoIn volunteerDtoIn) {
         var volunteer = new Volunteer();
         volunteer.setUsername(volunteerDtoIn.getUsername());
         volunteer.setFirstname(volunteerDtoIn.getFirstname());
         volunteer.setLastname(volunteerDtoIn.getLastname());
-        volunteer.setShelter();
+        volunteer.setShelter(shelterRepository.findById(volunteerDtoIn.getShelterId()).orElseThrow(RuntimeException::new));
         return volunteer;
     }
 
